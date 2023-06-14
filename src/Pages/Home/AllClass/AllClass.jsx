11853422d4b9b4aa9f23d600e9dc8b1d15faf36d
Badhon sarker <badhonsarker700@gmail.com>
2023-06-14 18:51:46 +0600

    class card added

--- src/Pages/Home/AllClass/AllClass.jsx
+++ src/Pages/Home/AllClass/AllClass.jsx
@@ -24,61 +24,7 @@ const AllClass = () => {
     })
   }, []);
 
-  // const handleSelectClass = (classId) => {
-  //   const cartItem = {menuItemId: _id, name, image, price}
-  //   if (user) {
-  //     fetch('https://yoga-zone-server-iota.vercel.app/ClassesCart',{
-  //       method:"POST",
-  //       headers:{
-  //         "content-type":"application/json"
-  //       },
-  //       body:JSON.stringify()
-  //     })
-  //       .then(res => res.json())
-  //       .then(data => {
-  //         console.log(data);
-  //         if (data.insertedId) {
-  //           Swal.fire({
-  //             icon: 'success',
-  //             title: 'Added successfully!',
-  //             showConfirmButton: false,
-  //             timer: 1500,
-  //           });
-            
-  //         }
-  //       })
-  //   }
-  //   else{
-  //     Swal.fire({
-  //       title: 'Please Login',
-  //       icon: 'warning',
-  //       showCancelButton: true,
-  //       confirmButtonColor: '#3085d6',
-  //       cancelButtonColor: '#d33',
-  //       confirmButtonText: 'Login Now'
-  //     }).then((result) => {
-  //       if (result.isConfirmed) {
-  //         // Swal.fire(
-  //         //   'Deleted!',
-  //         //   'Your file has been deleted.',
-  //         //   'success'
-  //         // )
-  //         navigate('/login',{state:{from: location}})
-  //       }
-  //     })
-  //   }
-  //   if (!isLoggedIn) {
-  //     alert('Please log in before selecting the course.');
-  //     return;
-  //   }
-
-  //   const selectedClass = classes.find((c) => c.id === classId);
-  //   if (selectedClass.availableSeats === 0 || isAdmin) {
-  //     return;
-  //   }
-  //   console.log('Selected class:', selectedClass);
-  // };
-
+  
   return (
     <div  className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
       {
