2d9d9b6436b67c30fc560541b28fbf9a7319175a
Badhon sarker <badhonsarker700@gmail.com>
2023-06-14 19:09:33 +0600

    dardkmood styles added:

--- src/Shared/DarkMode.jsx
+++ src/Shared/DarkMode.jsx
@@ -11,7 +11,9 @@ function DarkMode() {
 
   return (
     <div className={isDarkMode ? 'dark-mode' : 'light-mode'}>
-     
+      <button onClick={toggleTheme}>Toggle Dark Mode</button>
+      <h1 style={{ color: 'var(--text-color)' }}>Welcome to my website</h1>
+      <p style={{ color: 'var(--text-color)' }}>This is some content.</p>
     </div>
   );
 }
