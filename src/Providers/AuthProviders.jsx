16a41e3cb346915ed496accc41cfd8beda12452d
Badhon sarker <badhonsarker700@gmail.com>
2023-06-14 19:05:25 +0600

    authprovider added google provider

--- src/Providers/AuthProviders.jsx
+++ src/Providers/AuthProviders.jsx
@@ -1,7 +1,7 @@
 import {  createContext, useEffect, useState } from "react";
 import { GoogleAuthProvider, getAuth, onAuthStateChanged,createUserWithEmailAndPassword, signInWithEmailAndPassword, signInWithPopup, signOut, updateProfile } from "firebase/auth";
 import { app } from "../firebase/firebase.config";
-
+//authprovider
 export const AuthContext = createContext(null);
 const auth = getAuth(app);
 
