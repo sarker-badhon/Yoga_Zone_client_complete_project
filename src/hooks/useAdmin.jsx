ea3629c6d23e27a25c36880e03db7c117d7f8eb6
Badhon sarker <badhonsarker700@gmail.com>
2023-06-14 18:49:31 +0600

    useaxiossuceure hook added

--- src/hooks/useAdmin.jsx
+++ src/hooks/useAdmin.jsx
@@ -20,26 +20,3 @@ const useAdmin = () => {
 };
 
 export default useAdmin;
-
-// import { useQuery } from 'react-query';
-// import { useContext } from 'react';
-// import { AuthContext } from "../Providers/AuthProviders";
-// import useAxiosSecure from "./useAxiosSecure";
-
-// const useAdmin = () => {
-//   const { user, loading } = useContext(AuthContext);
-//   const [axiosSecure] = useAxiosSecure();
-
-//   const {data: isAdmin, isLoading: isAdminLoading} = useQuery({ 
-//       queryKey: ['isAdmin', user?.email],
-//       enabled: !loading,
-//       queryFn: async () => {
-//           const res = await axiosSecure.get(`/users/${user?.email}?role=admin`);
-//           return res.data;
-//       }
-//   })
-//   return [isAdmin, isAdminLoading]
-
-// };
-
-// export default useAdmin;
